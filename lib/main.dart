6db6ccef592ae3c2ed5a96b7f9303a816b3048ee
adi.boyii <adireyes00@gmail.com>
2022-05-03 15:09:49 +0800

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bmi_calculator/input_height.dart';
 import 'package:bmi_calculator/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/landing_page.dart';
+import 'package:bmi_calculator/input_height.dart';
+import 'package:bmi_calculator/input_weight.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -13,6 +16,8 @@ class BMICalculator extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => LandingPage(),
+        '/input_height': (context) => InputHeight(),
+        '/input_weight': (context) => InputWeight(),
       },
     );
   }
